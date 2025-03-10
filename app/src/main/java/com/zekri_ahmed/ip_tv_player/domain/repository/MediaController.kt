bfda100fd842f818a64db0af6237d0c63fe7650a
ahmed-zekri <ahmed.zekri.a@gmail.com>
2025-03-10 16:11:39 +0100

    Refactor: Replace PlayerState model with MediaController's StateFlow
    This commit refactors the `PlayerState` model by removing the dedicated `PlayerState` class and moving its properties into a `PlayerState` data class within the `MediaController` interface. The `getState()` function of `MediaController` now returns a `StateFlow<PlayerState>`, providing a more integrated and streamlined way to access the player's state.

--- app/src/main/java/com/zekri_ahmed/ip_tv_player/domain/repository/MediaController.kt
+++ app/src/main/java/com/zekri_ahmed/ip_tv_player/domain/repository/MediaController.kt
@@ -1,6 +1,5 @@
 package com.zekri_ahmed.ip_tv_player.domain.repository
 
-import com.zekri_ahmed.ip_tv_player.domain.model.PlayerState
 import kotlinx.coroutines.flow.StateFlow
 
 
@@ -11,3 +10,16 @@ interface MediaController {
     fun seekTo(position: Long)
     fun getState(): StateFlow<PlayerState>
 }
+
+data class PlayerState(
+    val player: Any? = null, // Expose the player here
+    val isPlaying: Boolean = false,
+    val currentPosition: Long = 0,
+    val bufferedPosition: Long = 0,
+    val duration: Long = 0,
+    val currentMediaUrl: String = "",
+    val title: String = "",
+    val isFullScreen: Boolean = false,
+    val isLoading: Boolean = false,
+    val playerError: String? = null
+)
